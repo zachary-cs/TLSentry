34b62c3491ff213441ec1094cf20191f9f7259bb
0zone <0zone4bove@protonmail.com>
2022-07-21 10:43:21 -0700

    Initial Python Files

---
+++ scanner/Scanner.py
@@ -0,0 +1,16 @@
+from ssl import *
+from cryptography import x509
+from socket import socket
+
+import idna
+
+class Scanner():
+    # Vars go here
+    self.endpoints = []
+
+    def __init__(self):
+        sock = socket()
+        
+        
+        
+
