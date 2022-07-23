12e4b3b478f6ad8ff4da2381738f0653b591cebc
0zone <0zone4bove@protonmail.com>
2022-07-23 15:06:48 -0700

    Moved out of SRC

---
+++ scanner/Scanner.py
@@ -0,0 +1,16 @@
+from ssl import *
+from cryptography import x509
+from socket import socket
+
+import idna
+
+class Scanner():
+    # Vars go here
+    Endpoints = []
+
+    def __init__(self):
+        sock = socket()
+        
+        
+        
+
