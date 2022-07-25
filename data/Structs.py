2842f30bbeab79a60c63dd8242baea953156ce38
0zone <0zone4bove@protonmail.com>
2022-07-24 22:32:03 -0700

    Cert class not needed, Endpoint fixes

--- data/Structs.py
+++ data/Structs.py
@@ -1,24 +1,12 @@
 from datetime import *
-
-class Certificate():
-    # Public Vars go here
-    Common_Name = ""
-
-    def __init__(self, cn):
-        # TODO - Add more init vars here
-        self.Common_Name = cn
-
+from OpenSSL import SSL
 
 # Object to encapsulate the endpoint
 class Endpoint():
-    URL = ""
-    Port = ""
-    IP = ""
 
-    def __init__(self, url, port, frequency):
+    def __init__(self, url, port):
         self.URL = url
         self.Port = port
-        self.Frequency = frequency
 
 # Put Variables that won't change here
 class Statics():
