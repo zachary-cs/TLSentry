14385ba99e990d2a04b50f35d3fed2ce3976ed93
zachary-cs <zachary.chunscott@gmail.com>
2022-08-01 20:27:57 -0700

    Page Data Structure

--- tlsentry/data/Structs.py
+++ tlsentry/data/Structs.py
@@ -1,6 +1,16 @@
 from datetime import *
 from OpenSSL import SSL
 
+
+
+class PageData():
+    # Public Static Vars
+    app_version = 0.1
+    title = "TLSentry - Endpoint SSL Monitoring"
+
+    def __init__(self):
+        pass
+
 class Certificate():
 
     def __init__(self, hostname, commonname, peername, san, issuer, notbefore, notafter):
