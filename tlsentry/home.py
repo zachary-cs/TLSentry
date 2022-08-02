0d145689688e42651feb6f554fa6d64c67cb0174
zachary-cs <zachary.chunscott@gmail.com>
2022-08-01 20:29:23 -0700

    Cleaning up Home Index

--- tlsentry/home.py
+++ tlsentry/home.py
@@ -12,26 +12,5 @@ home = Blueprint('home', __name__) # __name__ is essentially the main() of this 
 @home.get("/")
 @home.get("/index.html")
 def index(name=None):
-  page_data = {
-    'title' : "TLSentry - Endpoint SSL Monitoring",
-    'app_version' : 0.1
-  }
-  return render_template('home_index.j2', title=page_data["title"], app_version=page_data["app_version"])
-
-
-
-
-
-# TODO Scanning API
-@home.route('/scan', methods=['POST','GET'])
-def scan_urls():
-  if request.method == 'POST':
-    # TODO
-    return ""
-  else:
-    scanner = network.SSL_Scanner("www.google.com", 443)
-    # Get Dict object of Certificate
-    cert_dict = scanner.Certificate.GetDict()
-    return json.dumps(cert_dict)
-
-
+  data = Structs.PageData()
+  return render_template('home_index.j2', title=data.title, app_version=data.app_version)
