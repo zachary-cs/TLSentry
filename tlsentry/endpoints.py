5db59ac036a12786ebcae778e7b5dc5b16e72622
zachary-cs <zachary.chunscott@gmail.com>
2022-08-01 20:28:21 -0700

    Using new PageData

---
+++ tlsentry/endpoints.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import Blueprint
+from .utils import network
+from .data import Structs
+
+
+# Define this python file as the blueprint 
+certs = Blueprint('certs', __name__) # __name__ is essentially the main() of this python file
+
+# Index of Certs
+@certs.route("/")
+def hello(name):
+  return render_template('certs_index.j2', )
+
+
