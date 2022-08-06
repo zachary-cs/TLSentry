9735ce6a19644d1fd8c4453bccd8513118ae6d81
zachary-cs <zachary.chunscott@gmail.com>
2022-08-06 16:52:51 -0700

    Endpoints Working

--- tlsentry/endpoints.py
+++ tlsentry/endpoints.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 from flask import Blueprint
 from .utils import network
+from .utils import db_connector
 from .data import Structs
 
 
@@ -12,7 +13,19 @@ endpoints = Blueprint('endpoints', __name__) # __name__ is essentially the main(
 # Index of endpoints
 @endpoints.route("/")
 def index():
-  page_data = Structs.PageData()
-  return render_template('endpoints_index.j2', pagedata=page_data)
+  pagedata = Structs.PageData()
+  
+  # Run Query and get results
+  db_conn = db_connector.MySQL_Connector()
+  q_headers, q_results = db_conn.Run_Query("select * from endpoints")
 
+  # Render
+  return render_template(
+                          'endpoints_index.j2', 
+                          pagedata=pagedata, 
+                          sub_title="Endpoints",
+                          table_title="Node Endpoints",
+                          headers=q_headers, 
+                          results=q_results
+                        )
 
