e8f61567eb850800b5f8a1715cdb673b04aa523a
zachary-cs <zachary.chunscott@gmail.com>
2022-08-24 21:08:54 -0700

    Network Scanner Improvements

--- tlsentry/utils/network.py
+++ tlsentry/utils/network.py
@@ -9,15 +9,65 @@ from ..data import Structs
 
 
 
-class Scanner():    
+class Scanner():   
+
+
     
     def __init__(self, hostname, port):
-        # Pull the SSL Cert from the Hostname:Port 
-        self.Certificate = self.Obtain_Certificate(hostname, port)
-        self.Crypto_Cert = self.Obtain_Crypto_Certificate(hostname, port)
+
+        # Public
+        self.Certificate = None
+        self.Endpoint = None
+
+        # Pull the SSL Cert from the Hostname:Port
+        self.ScanHost(hostname, port)
+        self.Crypto_Cert = self.Do_Scan(hostname, port)
+
         # TODO - Pull IP, response time, other info here...
 
+
+    def ScanHost(self, hostname, port):
+        idna_hostname = idna.encode(hostname)
+
+        # TODO - Better error handling for bad hostname/ports
+
+        # Setup socket connection
+        sock = socket()
+        sock.connect((hostname, port))
+        peername = sock.getpeername()
+        ctx = SSL.Context(SSL.SSLv23_METHOD)
+        ctx.check_hostname = False
+        ctx.verify_mode = SSL.VERIFY_NONE
+        # Do Handshake and acquire Cert
+        sock_ssl = SSL.Connection(ctx, sock)
+        sock_ssl.set_connect_state()
+        sock_ssl.set_tlsext_host_name(idna_hostname)
+        sock_ssl.do_handshake()
+        cert = sock_ssl.get_peer_certificate()
         
+        # Check for null cert
+        if cert is None:
+            return
+        else:
+            crypto_cert = cert.to_cryptography()
+            # Close down
+            sock_ssl.close()
+            sock.close()
+
+        # Setup the Certificate Object
+        self.Certificate = Structs.Certificate(
+                                    hostname,
+                                    self.Get_Common_Name(crypto_cert), 
+                                    peername,
+                                    self.Get_Alt_Names(crypto_cert),
+                                    self.Get_Issuer(crypto_cert),
+                                    crypto_cert.not_valid_before,
+                                    crypto_cert.not_valid_after
+                                )
+        # Setup the Endpoint Object
+        self.Endpoint = Structs.Endpoint(url, port)
+
+    # Helper Functions        
     def Get_Certificate(self):
         return self.Certificate
         
@@ -27,70 +77,6 @@ class Scanner():
         # service_identity.pyopenssl.verify_hostname(client_ssl, hostname)
         # issuer
 
-
-    def Obtain_Certificate(self, hostname, port):
-        idna_hostname = idna.encode(hostname)
-
-        # TODO - Better error handling for bad hostname/ports
-
-        # Setup socket connection
-        sock = socket()
-        sock.connect((hostname, port))
-        peername = sock.getpeername()
-        ctx = SSL.Context(SSL.SSLv23_METHOD)
-        ctx.check_hostname = False
-        ctx.verify_mode = SSL.VERIFY_NONE
-        # Do Handshake and acquire Cert
-        sock_ssl = SSL.Connection(ctx, sock)
-        sock_ssl.set_connect_state()
-        sock_ssl.set_tlsext_host_name(idna_hostname)
-        sock_ssl.do_handshake()
-        cert = sock_ssl.get_peer_certificate()
-        crypto_cert = cert.to_cryptography()
-        # Close down
-        sock_ssl.close()
-        sock.close()
-
-        # Setup the Certificate Object
-        certificate = Structs.Certificate(
-                                    hostname,
-                                    self.Get_Common_Name(crypto_cert), 
-                                    peername,
-                                    self.Get_Alt_Names(crypto_cert),
-                                    self.Get_Issuer(crypto_cert),
-                                    crypto_cert.not_valid_before,
-                                    crypto_cert.not_valid_after,
-                                    hashlib.sha1()
-                                )
-        # TODO - return null if missing Cert
-        return certificate
-
-    def Obtain_Crypto_Certificate(self, hostname, port):
-        idna_hostname = idna.encode(hostname)
-
-        # TODO - Better error handling for bad hostname/ports
-
-        # Setup socket connection
-        sock = socket()
-        sock.connect((hostname, port))
-        peername = sock.getpeername()
-        ctx = SSL.Context(SSL.SSLv23_METHOD)
-        ctx.check_hostname = False
-        ctx.verify_mode = SSL.VERIFY_NONE
-        # Do Handshake and acquire Cert
-        sock_ssl = SSL.Connection(ctx, sock)
-        sock_ssl.set_connect_state()
-        sock_ssl.set_tlsext_host_name(idna_hostname)
-        sock_ssl.do_handshake()
-        cert = sock_ssl.get_peer_certificate()
-        crypto_cert = cert.to_cryptography()
-        # Close down
-        sock_ssl.close()
-        sock.close()
-
-        return cert
-
-    # Helper Functions
     def Get_Alt_Names(self, cert):
         try:
             ext = cert.extensions.get_extension_for_class(x509.SubjectAlternativeName)
